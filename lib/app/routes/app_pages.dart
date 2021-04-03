7e64d6d1ec2d18d1cda0aa781ac373645037c6a6
santo-polibatam <santo@polibatam.ac.id>
2021-04-03 19:55:51 +0700

    tambah pages

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,16 +1,22 @@
 import 'package:get/get.dart';
-
-import 'package:polibatam_one/app/modules/SignUp/bindings/sign_up_binding.dart';
-import 'package:polibatam_one/app/modules/SignUp/views/sign_up_view.dart';
+import 'package:polibatam_one/app/modules/Aunth/SignUp/bindings/sign_up_binding.dart';
+import 'package:polibatam_one/app/modules/Aunth/SignUp/views/sign_up_view.dart';
+import 'package:polibatam_one/app/modules/Aunth/login/bindings/login_binding.dart';
+import 'package:polibatam_one/app/modules/Aunth/login/views/login_view.dart';
+import 'package:polibatam_one/app/modules/Kepegawaian/AbsensiKaryawan/bindings/kepegawaian_absensi_karyawan_binding.dart';
+import 'package:polibatam_one/app/modules/Kepegawaian/AbsensiKaryawan/views/kepegawaian_absensi_karyawan_view.dart';
+import 'package:polibatam_one/app/modules/Keuangan/Gaji/bindings/keuangan_gaji_binding.dart';
+import 'package:polibatam_one/app/modules/Keuangan/Gaji/views/keuangan_gaji_view.dart';
+import 'package:polibatam_one/app/modules/Perkuliahan/AbsensiKuliah/bindings/perkuliahan_absensi_kuliah_binding.dart';
+import 'package:polibatam_one/app/modules/Perkuliahan/AbsensiKuliah/views/perkuliahan_absensi_kuliah_view.dart';
+import 'package:polibatam_one/app/modules/Splash/bindings/splash_binding.dart';
+import 'package:polibatam_one/app/modules/Splash/views/splash_view.dart';
 import 'package:polibatam_one/app/modules/home/bindings/home_binding.dart';
 import 'package:polibatam_one/app/modules/home/views/home_view.dart';
-import 'package:polibatam_one/app/modules/login/bindings/login_binding.dart';
-import 'package:polibatam_one/app/modules/login/views/login_view.dart';
-
 part 'app_routes.dart';
 
 class AppPages {
-  static const INITIAL = Routes.LOGIN;
+  static const INITIAL = Routes.SPLASH;
 
   static final routes = [
     GetPage(
@@ -27,6 +33,26 @@ class AppPages {
       name: _Paths.SIGN_UP,
       page: () => SignUpView(),
       binding: SignUpBinding(),
+    ),
+    GetPage(
+      name: _Paths.SPLASH,
+      page: () => SplashView(),
+      binding: SplashBinding(),
+    ),
+    GetPage(
+      name: _Paths.KEPEGAWAIAN_ABSENSI_KARYAWAN,
+      page: () => KepegawaianAbsensiKaryawanView(),
+      binding: KepegawaianAbsensiKaryawanBinding(),
+    ),
+    GetPage(
+      name: _Paths.KEUANGAN_GAJI,
+      page: () => KeuanganGajiView(),
+      binding: KeuanganGajiBinding(),
+    ),
+    GetPage(
+      name: _Paths.PERKULIAHAN_ABSENSI_KULIAH,
+      page: () => PerkuliahanAbsensiKuliahView(),
+      binding: PerkuliahanAbsensiKuliahBinding(),
     ),
   ];
 }
